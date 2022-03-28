24002aae43504ac2738a198f1ee5fe4dcab6ca8a
Ming-der Wang <mingderwang@gmail.com>
2022-03-28 17:08:46 +0800

    use ming custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,24 @@ module.exports = {
 
   content: [],
   daisyui: {
-    themes: ["cyberpunk", "dark", "light"],
+    themes: [
+      {
+        mingtheme: {
+          primary: "#ea580c",
+          secondary: "#4084dd",
+          accent: "#84cc16",
+          neutral: "#171E21",
+          "base-100": "#F4F4F6",
+          info: "#2165ED",
+          success: "#2dd4bf",
+          warning: "#fde047",
+          error: "#e11d48",
+        },
+      },
+      "cyberpunk",
+      "dark",
+      "light",
+    ],
   },
   theme: {
     extend: {},
