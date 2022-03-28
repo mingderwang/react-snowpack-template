e76cfe54a7165560d88bb81294e337f1843372b4
Ming-der Wang <mingderwang@gmail.com>
2022-03-28 16:35:02 +0800

    add App.jsx with HMR

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,10 @@
 /* Add JavaScript code here! */
 import React from "react";
 import ReactDOM from "react-dom";
--ReactDOM.render(<div>"HELLO WORLD"</div>, document.getElementById("root"));
+import App from "./App.jsx";
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
